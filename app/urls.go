d5ff40fa7f909e4354fcdcabbfabe71345b69750
suthanthdk <suthuganesha.dk@gmail.com>
2021-04-11 19:11:11 +0530

    Initialized the go web project

---
+++ app/urls.go
@@ -0,0 +1,11 @@
+package app
+
+import "github.com/suthanth/bookstore_users_api/controllers"
+
+func mapUrls() {
+	router.GET("/ping", controllers.Ping)
+
+	router.GET("/users/:user_id", controllers.GetUser)
+	router.GET("/users/search", controllers.SearchUser)
+	router.POST("/users", controllers.CreateUser)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/suthanth/bookstore_users_api
+
+go 1.16
+
+require github.com/gin-gonic/gin v1.7.1
